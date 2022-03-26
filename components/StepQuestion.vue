64ae7ee0667f06e657957f98ff1744b12d82c74d
ayuki_j <ajjt66554@gmail.com>
2022-03-26 12:24:46 +0900

    create StepQuestion.vue

---
+++ components/StepQuestion.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-stepper v-model="current">
+    <v-stepper-items>
+      <v-stepper-content
+        v-for="(question, index) in questions"
+        :key="index"
+        :step="index"
+      >
+        <v-card>
+          <v-img contain height="300" :src="question.image"></v-img>
+          <v-card-title class="mt-10 py-10 text-center">
+            {{ question.question }}
+          </v-card-title>
+          <v-card-actions class="mt-10 pb-10">
+            <v-row>
+              <v-col class="d-flex justify-center">
+                <v-btn color="primary" large @click="nextQuestion">
+                  はい
+                </v-btn>
+              </v-col>
+              <v-col class="d-flex justify-center">
+                <v-btn color="error" large @click="nextQuestion">
+                  いいえ
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  name: "StepQuestion",
+  props: {
+    questions: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      current: 0,
+    }
+  },
+  methods: {
+    nextQuestion() {
+      this.current++
+      if (this.current > this.questions.length - 1) {
+        this.$router.push("problem-presentation")
+      }
+    },
+  },
+}
+</script>
+
+<style scoped></style>
--- pages/simple-question.vue
+++ pages/simple-question.vue
@@ -1,36 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="10" md="8">
-      <v-stepper v-model="current">
-        <v-stepper-items>
-          <v-stepper-content
-            v-for="(question, index) in questions"
-            :key="index"
-            :step="index"
-          >
-            <v-card>
-              <v-img contain height="300" :src="question.image"></v-img>
-              <v-card-title class="mt-10 py-10 text-center">
-                {{ question.question }}
-              </v-card-title>
-              <v-card-actions class="mt-10 pb-10">
-                <v-row>
-                  <v-col class="d-flex justify-center">
-                    <v-btn color="primary" large @click="nextQuestion">
-                      はい
-                    </v-btn>
-                  </v-col>
-                  <v-col class="d-flex justify-center">
-                    <v-btn color="error" large @click="nextQuestion">
-                      いいえ
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-card-actions>
-            </v-card>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
+      <step-question :questions="questions"></step-question>
     </v-col>
   </v-row>
 </template>
@@ -67,29 +38,7 @@ export default {
             "2020年度の日本国内の年間発電電力量に占める自然エネルギーの割合は21%に達した",
         },
       ],
-      questionNumber: 0,
-      questionText: "",
-      questionImage: "",
-      questionAfterText: "",
-      current: 0,
     }
-  },
-  created() {
-    this.init()
-  },
-  methods: {
-    init() {
-      this.questionText = this.questions[this.questionNumber].question
-      this.questionImage = this.questions[this.questionNumber].question
-      this.questionAfterText = this.questions[this.questionNumber].question
-    },
-    nextQuestion() {
-      this.current++
-      if (this.current > this.questions.length - 1) {
-        this.$router.push("problem-presentation")
-      }
-    },
-    beforeQuestion() {},
   },
 }
 </script>
