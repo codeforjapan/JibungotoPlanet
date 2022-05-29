59261688ed252d4ebbfc4273e101108f3d8ce62f
naoji3x <naoji.3x@gmail.com>
2022-05-29 09:47:32 +0900

    add aws-amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "jibungotoplanet": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
