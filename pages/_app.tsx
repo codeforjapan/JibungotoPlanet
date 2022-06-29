99bb8dcd8a54c3eb226339b139fa8f36124c9918
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-06-29 14:18:16 +0900

    run lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { chakraTheme } from '../utils/chakratheme'
 import { RecoilRoot } from 'recoil'
+import { chakraTheme } from '../utils/chakratheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
