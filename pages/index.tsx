50fe780a0952b45323e645e7b11557a17029d26f
kur00 <over.y31.bsk@gmail.com>
2022-07-10 22:13:10 +0900

    wip: created home header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import { FC } from 'react'
-import BasicButton from '../components/atoms/buttons/Basic'
-import SelectBox from '../components/atoms/inputs/Select'
+import HomeHeader from 'components/organisms/homes/HomeHeader/HomeHeader'
 
 const Home: FC = () => {
   return (
     <div>
-      <BasicButton isNext>次の質問へ</BasicButton>
-      <SelectBox
-        onChange={() => {}}
-        options={[{ value: 'test', label: 'テスト' }]}
-      ></SelectBox>
+      <HomeHeader />
     </div>
   )
 }
