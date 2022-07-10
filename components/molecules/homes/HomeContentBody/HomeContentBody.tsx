a304f291d609e2a70446ed0cb39e10ea12a09016
kur00 <over.y31.bsk@gmail.com>
2022-07-10 17:05:02 +0900

    feat: added home content body

---
+++ components/molecules/homes/HomeContentBody/HomeContentBody.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode } from "react";
+import { Text } from '@chakra-ui/react'
+
+type Props = {
+  className?: string;
+  body?: ReactNode;
+};
+
+const HomeContentBody: FC<Props> = (props) => {
+  const baseStyle = {
+    fontSize: '18px',
+    lineHeight: '190%',
+    letterSpacing: '0.05em',
+    fontWeight: 500,
+    color: '#333333'
+  }
+
+  return (
+    <Text style={ baseStyle }>{props.body}</Text>
+  );
+};
+
+export default HomeContentBody;
