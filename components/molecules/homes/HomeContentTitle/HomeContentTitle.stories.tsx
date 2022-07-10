cb61ddb7efd4b02d20c5cf00bb25883f257ca801
kur00 <over.y31.bsk@gmail.com>
2022-07-10 16:27:15 +0900

    feat: added home content title

---
+++ components/molecules/homes/HomeContentTitle/HomeContentTitle.stories.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/named
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import HomeContentTitle from './HomeContentTitle';
+
+export default {
+  title: 'components / atoms / buttons / HomeContentTitle',
+  component: HomeContentTitle,
+} as ComponentMeta<typeof HomeContentTitle>;
+
+export const Index: ComponentStoryObj<typeof HomeContentTitle> = {
+  args: {
+   title: '1. 知る'
+  },
+};
---
+++ components/molecules/homes/HomeContentTitle/HomeContentTitle.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { Heading } from '@chakra-ui/react'
+
+type Props = {
+  className?: string;
+  title?: string;
+};
+
+const HomeContentTitle: FC<Props> = (props) => {
+  const baseStyle = {
+    fontSize: '62px',
+    lineHeight: '160%',
+    letterSpacing: '0.2em',
+    fontWeight: 700
+  }
+
+  return (
+    <Heading style={ baseStyle } as='h3'>{props.title}</Heading>
+  );
+};
+
+export default HomeContentTitle;
