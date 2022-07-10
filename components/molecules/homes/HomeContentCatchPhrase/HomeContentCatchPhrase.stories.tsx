8a0c291c6552ef0a763a7cbb77275a58a470e315
kur00 <over.y31.bsk@gmail.com>
2022-07-10 16:44:52 +0900

    feat: added home content catch phrase

---
+++ components/molecules/homes/HomeContentCatchPhrase/HomeContentCatchPhrase.stories.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/named
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react';
+import HomeContentCatchPhrase from './HomeContentCatchPhrase';
+
+export default {
+  title: 'components / atoms / buttons / HomeContentCatchPhrase',
+  component: HomeContentCatchPhrase,
+} as ComponentMeta<typeof HomeContentCatchPhrase>;
+
+export const Index: ComponentStoryObj<typeof HomeContentCatchPhrase> = {
+  args: {
+    catchPhrase: <span>まずはあなたの現状を知ることからはじめ、<br />ひとりひとりの具体的な気候変動対策を</span>
+  },
+};
