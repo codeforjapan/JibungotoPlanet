11ff310d8f747bc15d1906588344c543e603393f
kur00 <over.y31.bsk@gmail.com>
2022-07-23 14:58:27 +0900

    feat: created home footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { Container } from '@chakra-ui/react'
 import HomeCaptionSection from 'components/organisms/homes/HomeCaptionSection/HomeCaptionSection'
 import HomeChangeSection from 'components/organisms/homes/HomeChangeSection/HomeChangeSection'
+import HomeFooter from 'components/organisms/homes/HomeFooter/HomeFooter'
 import HomeHeader from 'components/organisms/homes/HomeHeader/HomeHeader'
 import HomeKnowSection from 'components/organisms/homes/HomeKnowSection/HomeKnowSection'
 import HomeLookBackSection from 'components/organisms/homes/HomeLookBackSection/HomeLookBackSection'
@@ -19,6 +20,7 @@ const Home: FC = () => {
         <HomeChangeSection className={styles['home__section']} />
         <HomeCaptionSection className={styles['home__caption-section']} />
       </Container>
+      <HomeFooter />
     </div>
   )
 }
