9204f539d26a40f805c92fc6f2ebf9fe8544a7ce
kur00 <over.y31.bsk@gmail.com>
2022-07-23 12:50:56 +0900

    feat: create home caption section

---
+++ components/organisms/homes/HomeCaptionSection/HomeCaptionSection.stories.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/named
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import HomeCaptionSection from './HomeCaptionSection'
+
+export default {
+  title: 'components / organisms / homes / HomeCaptionSection',
+  component: HomeCaptionSection
+} as ComponentMeta<typeof HomeCaptionSection>
+
+export const Index: ComponentStoryObj<typeof HomeCaptionSection> = {
+  args: {}
+}
