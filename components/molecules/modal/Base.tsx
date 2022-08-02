0b6655ff6d2c59bdaa459649cc1dd5b5479efebc
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-08-02 23:00:05 +0900

    create modal base

---
+++ components/molecules/modal/Base.tsx
@@ -0,0 +1,36 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Box
+} from '@chakra-ui/react'
+import { FC, ReactNode, useRef } from 'react'
+
+type Props = {
+  isOpen: boolean
+  onClose: () => void
+  children?: ReactNode
+}
+
+const ModalBase: FC<Props> = ({ isOpen, onClose, children }) => {
+  const cancelRef = useRef<any>()
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      onClose={() => onClose()}
+      leastDestructiveRef={cancelRef}
+      isCentered
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent mx={2} pb={3}>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>{children}</AlertDialogBody>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
+
+export default ModalBase
