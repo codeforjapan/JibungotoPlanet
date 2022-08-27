5fe32a5ac6a1c1b2a4618b6a72d3ef77d82d0af4
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-08-27 09:05:51 +0900

    fix calc estimation and don't show 0 emission subdomain

--- @types/profile.d.ts
+++ @types/profile.d.ts
@@ -1,10 +1,10 @@
 declare namespace Profile {
   interface Profile {
-    actionIntensityRates: Actions.ActionIntensityRate[];
+    actionIntensityRates: Actions.ActionIntensityRate[]
     id: string
     shareId: string
-    baselines: Baseline[]
-    estimations: Estimation[]
+    baselines: EmissionItem[]
+    estimations: EmissionItem[]
     createdAt: Date
     updatedAt: Date
     mobilityAnswer?: any
@@ -16,22 +16,12 @@ declare namespace Profile {
     region?: string
   }
 
-  interface Baseline {
-    citations: string
+  interface EmissionItem {
     domain: Questions.QuestionCategory
     item: string
     subdomain: string
     type: 'amount' | 'intensity'
     unit: string
     value: value
-  }
-
-  interface Estimation {
-    domain: Questions.QuestionCategory
-    item: string
-    subdomain: string
-    type: 'amount' | 'intensity'
-    unit: string
-    value: number
   }
 }
