c56be197dfc6b24760b8f02dd9aea54d4981d3c7
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-08-29 21:33:44 +0900

    lint fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import TagManager from 'react-gtm-module'
 import { RecoilRoot } from 'recoil'
 import HeadElm from 'components/HeadElm'
-import { chakraTheme } from 'utils/chakratheme'
-import { useEffect } from 'react'
-import TagManager from 'react-gtm-module'
 import Cookie from 'components/molecules/homes/Cookie/Cookie'
+import { chakraTheme } from 'utils/chakratheme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
