c3dc4679420777a768cbff01556abeb71bbbfedc
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-09-09 17:45:54 +0900

    refactor: format

--- @types/actions.d.ts
+++ @types/actions.d.ts
@@ -1,21 +1,21 @@
 declare namespace Actions {
   interface Action {
-    id: number;
-    domain: string;
-    option: string;
-    category: ActionCategory;
-    label: string; // アクションラベル
-    description: string; //説明
-    reductionEffect: number;
+    id: number
+    domain: string
+    option: string
+    category: ActionCategory
+    label: string // アクションラベル
+    description: string //説明
+    reductionEffect: number
     actionIntensityRate: ActionIntensityRate | null // 標準の削減施策の実施率 apiから取得
-    checked: boolean; // 選択されたかどうか
+    checked: boolean // 選択されたかどうか
   }
 
   type ActionCategory = 'food' | 'mobility' | 'housing' | 'other'
   type ActionIntensityRate = {
-    range: number[],
-    value: number,
-    defaultValue: number,
+    range: number[]
+    value: number
+    defaultValue: number
     option: string
   }
 }
