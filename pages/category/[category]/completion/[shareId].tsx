bf51a27504a8feae5c7a920fbaac6333d8e04b64
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-09-28 03:15:56 +0900

    fix: share error

--- pages/category/[category]/completion/[shareId].tsx
+++ pages/category/[category]/completion/[shareId].tsx
@@ -18,7 +18,7 @@ const CompletionPage: NextPage<Params> = ({ category, shareId }) => {
 
   return (
     <QuestionContainer category={category}>
-      <CompletionHeader category={category} />
+      <CompletionHeader category={category} shareId={shareId} />
       <CompletionContent category={category} shareId={shareId} />
       <Box pt={14}>
         <BasicButton
